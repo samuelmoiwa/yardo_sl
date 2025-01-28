3333f610207b30c11147737ee41c960ea53b45a6
Moiwa <samuelmoiwa@gmail.com>
2025-01-28 14:44:34 +0000

    update

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -4,6 +4,7 @@ import logoLight from "./logo-light.svg";
 export function Welcome() {
   return (
     <div className="flex w-full justify-center">
+      
       <p className="text-red-400"> Hello world </p>
     </div>
   );
