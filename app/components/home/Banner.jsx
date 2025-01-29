74bcfdf137d8e7ac4c50e096813ea8618669dbfa
Moiwa <samuelmoiwa@gmail.com>
2025-01-29 08:56:55 +0000

    update

--- app/components/home/Banner.jsx
+++ app/components/home/Banner.jsx
@@ -7,11 +7,11 @@ const Lottie = lazy(() => import('lottie-react'));
 
 const Banner = () => {
   return (
-    <section className="w-full flex justify-center items-center py-12">
+    <section className="w-full flex justify-center items-center py-12 dark:bg-gray-950 bg-blue-950">
       <div className="container mx-auto px-4 md:px-8 lg:px-16 flex flex-col md:flex-row-reverse items-center gap-8 h-[30rem] max-w-[90rem]">
         {/* Text Section */}
         <div className="space-y-6 md:w-1/2">
-          <h2 className="text-3xl lg:text-4xl font-bold text-blue-800">
+          <h2 className="text-3xl lg:text-4xl font-bold dark:text-blue-800 text-blue-400">
           Empowering Youth, <br/> Transforming Communities
           </h2>
           <div className="space-y-4">
@@ -19,7 +19,7 @@ const Banner = () => {
             <div className="flex items-start gap-4">
               <FaSeedling className="text-green-700 text-4xl" />
               <div>
-                <p className="text-gray-600 dark:text-gray-400 text-sm md:text-base leading-relaxed">
+                <p className="text-gray-300 dark:text-gray-400 text-sm md:text-base leading-relaxed">
                 Welcome to YARDO-SL, a youth empowerment agency dedicated to creating a just and inclusive society
                 free from poverty, hunger, environmental degradation, disease, and inequality.
                 </p>
