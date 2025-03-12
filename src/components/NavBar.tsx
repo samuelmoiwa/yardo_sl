d0a934759a244a37c9cb8661843857d3ce3a67ae
Moiwa <samuelmoiwa@gmail.com>
2025-03-12 14:39:35 +0000

    update

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -207,10 +207,55 @@ const NavBar: React.FC = () => {
 
             </div>
           </div>
+        </div>
+      </div>
+
+
+      {isOpen && (
+        <div className="lg:hidden flex flex-col justify-center items-center px-3 pb-3">
+          <div className="pt-2 pb-3 flex flex-col justify-center items-center">
+            {navigation.map((item) => (
+              <div key={item.name}>
+                <NavLink
+                  to={item.href}
+                  className={`block px-3 py-2 text-sm text-white hover:underline hover:decoration-blue-500 transition duration-150 ease-in-out font-Poppins font-semibold ${
+                    activeNav === item.href ? "underline decoration-blue-500 decoration-2" : ""
+                  }`}
+                  onClick={() => handleNavClick(item.href)}
+                >
+                  {item.name}
+                </NavLink>
+                {item.subLinks && item.subLinks.length > 0 && renderSubLinks(item.subLinks)}
+              </div>
+            ))}
+          </div>
+
+          <Link
+            to="#"
+            className="w-full text-white text-sm font-bold bg-yellow-500 hover:bg-yellow-400
+                        px-3 py-1 rounded text-center"
+          >
+            Make a Donation
+          </Link>
+
+          <button onClick={toggleTheme} className="text-white text-xl w-full flex flex-col justify-between items-center mt-4">
+            {theme === "dark" ?
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 text-white">
+            <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
+            </svg>
+
+            :
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+              className="size-6 text-white">
+              <path fillRule="evenodd" d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clipRule="evenodd" />
+            </svg>
+
+            }
+          </button>
 
 
         </div>
-      </div>
+      )}
     </nav>
     </div>
 
