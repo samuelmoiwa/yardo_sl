c72aecae199012fc80ada07215b3b78b5469833b
Moiwa <samuelmoiwa@gmail.com>
2025-03-13 21:36:31 +0000

    update

--- src/pages/programs.tsx
+++ src/pages/programs.tsx
@@ -6,8 +6,9 @@ const Programs = () => {
   return (
     <div className='w-full flex flex-col justify-center items-center bg-white dark:bg-slate-900'>
       <div className="w-full flex flex-col justify-center items-center bg-gradient-to-br from-blue-100 to-indigo-50
-      dark:from-gray-900 dark:to-indigo-950 bg-center bg-cover bg-no-repeat lg:pb-0 pb-10 overflow-hidden
-      max-w-[110rem]">
+        dark:from-gray-900 dark:to-indigo-950 bg-center bg-cover bg-no-repeat lg:pb-0 pb-10 overflow-hidden
+        max-w-[110rem]"
+      >
         <ProgramsHeader />
       </div>
     </div>
